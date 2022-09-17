b5dc953d4b2970f1776dee8cdc3f57afcc1de7de
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-17 09:59:12 +0600

    added Math

---
+++ math.js
@@ -0,0 +1,26 @@
+// practice :01 math, to the power!
+const mathPow = Math.pow(2, 5); // 2 to the power 5
+console.log(mathPow);
+
+
+
+// practice :02 math, Only absulute value!
+const mathAbs = Math.abs(23 - 43) // sudu absulute value t dibe ( + or - ) value dibe na!
+console.log(mathAbs);
+// practice :03 math, Round value dibe
+const mathRound = Math.round(20.5) // .5 er nise hole niser value |  .49+ er besi hole uporer value dibe.
+console.log(mathRound);
+// practice :04 math, ceil> uporer Vlue dibe
+const mathCeil = Math.ceil(2.00000001) // VognangSho er uporer value dibe
+console.log(mathCeil);
+// practice :05 math, flore >  niser value dibe !
+const mathFlore = Math.floor(49.9) // // VognangSho er niser value dibe
+console.log(mathFlore);
+// practice :06 math, random Number dibe >> loterir moto
+const mathRandom = Math.random() * 100 // (0 theke 100 ) er modde random number dite thakbe
+for (let i = 0; i < 24; i++) {
+
+    const mathLudu = Math.round(Math.random() * 6)
+    console.log(mathLudu);
+}
+// practice :07 math, to the power!
