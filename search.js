4baa013e7a205dc807e6186fc6051d3168707c45
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-17 07:31:51 +0600

    includes(),indexOf,StartWith,endWidth

---
+++ search.js
@@ -0,0 +1,5 @@
+const BiyarLyrics = 'Shishir veJa SOBUJ GASHE SURJO JOKON misti KasE toKon biYa koRai dImu';
+const forIncludes = BiyarLyrics.includes('Misti');
+const file = 'module.pdf'
+const BiyarLyricsStart= BiyarLyrics.toLowerCase().startsWith('shishir')
+console.log(file.endsWith('.pdf'));
